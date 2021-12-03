766a92ddfd1ce25f8765623ed5ad5d140946d304
bahadirg <fbahadirg@gmail.com>
2021-12-03 19:50:48 +0300

    JUnit4 removed

--- src/test/java/com/sampleapp/test/SampleBootProjectApplicationTests.java
+++ src/test/java/com/sampleapp/test/SampleBootProjectApplicationTests.java
@@ -1,7 +1,6 @@
-package com.sampleapp;
+package com.sampleapp.test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 //@SpringBootTest bootstrap the full application context, so we can @Autowire 
 // alternatifi @TestConfiguration, uygulamadakinden farkli test ortam tanimliyorsun, not: bunlar excluded from component scanning, @Import must be used
